4e28b028f1654732a0442b83ff035d1ce37d444a
Godmode Galactus <godmodegalactus@gmail.com>
2023-05-14 14:13:57 +0200

    Fixing start validator script and adding stake a validator script

---
+++ stake-a-validator.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+if [ -z "$1" ]
+  then
+      cat <<EOF
+
+usage: $0 VALIDATOR_VOTE_PUBKEY STAKE_AMOUNT [RPC_URL]
+
+Stake a validator
+
+VALIDATOR_VOTE_PUBKEY could be found by command \`solana validators\`
+STAKE_AMOUNT should be integer on number of sols to stake
+RPC_URL will be defaulted to http://127.0.0.1:8899
+
+Stakes will be updated when epoch ends.
+
+EOF
+  exit 1
+fi
+
+VALIDATOR_VOTE_PUBKEY=$1
+STAKE_AMOUNT=$2
+if ! [[ "$STAKE_AMOUNT" =~ ^[0-9]+$ ]]
+    then
+        echo "Sorry integers only"
+        exit 1
+fi
+
+rpc_url=$3
+if [[ -z $rpc_url ]]; then
+rpc_url="http://127.0.0.1:8899"
+fi
+
+solana-keygen new --no-passphrase -o tmp.json
+solana create-stake-account tmp.json $STAKE_AMOUNT -k random_user.json -u $rpc_url
+stake_addr=$(solana address -k tmp.json)
+rm tmp.json
+
+echo "staking to $VALIDATOR_VOTE_PUBKEY"
+
+solana delegate-stake $stake_addr $VALIDATOR_VOTE_PUBKEY -k random_user.json -u $rpc_url
+
+echo "stakes will be valid after"
+solana epoch-info
--- start-validator.sh
+++ start-validator.sh
@@ -1,20 +1,48 @@
 #!/bin/bash
+if [ -z "$1" ]
+  then
+      cat <<EOF
 
+usage: $0 ID [cluster entry point hostname] [solana validator arguments]
+
+Start a validator with no stake
+
+Will create a new validator in folder validator_{ID}
+Cluster entry point name will be defaulted to [127.0.0.1] and 
+gossip portrange will be defaulted to [8000 + 100 * {ID} - 8000 + 100 * {ID} + 50 ]
+
+EOF
+  exit 1
+fi
+
+
+ID=$1
+if ! [[ "$ID" =~ ^[0-9]+$ ]]
+    then
+        echo "Sorry integers only"
+        exit 1
+fi
+
+VALIDATOR_DIR=validator_$ID
 mkdir validator
-solana-keygen new --no-passphrase -o validator/identity.json
-solana-keygen new --no-passphrase -o validator/vote.json
-solana-keygen new --no-passphrase -o validator/withdrawer.json
-solana-keygen new --no-passphrase -o validator/stake.json
+solana-keygen new --no-passphrase -o $VALIDATOR_DIR/identity.json
+solana-keygen new --no-passphrase -o $VALIDATOR_DIR/vote.json
+solana-keygen new --no-passphrase -o $VALIDATOR_DIR/withdrawer.json
+solana-keygen new --no-passphrase -o $VALIDATOR_DIR/stake.json
 solana-keygen new --no-passphrase -o random_user.json
 
-entrypoint_hostname="$1"
+entrypoint_hostname="$2"
 echo "host : $entrypoint_hostname"
 if [[ -z $entrypoint_hostname ]]; then
 gossip_entrypoint=127.0.0.1:8001
+let start_port=8000+$ID\*100
+let end_port=8000+$ID\*100+50
+dynamic_port_range=$"$start_port-$end_port"
 else
 gossip_entrypoint="$entrypoint_hostname":8001
+dynamic_port_range="8001:8050"
 fi
-echo "gossip : $gossip_entrypoint"
+echo "gossip : $gossip_entrypoint port range $dynamic_port_range"
 
 rpc_url=$(solana-gossip --allow-private-addr rpc-url --timeout 180 --entrypoint "$gossip_entrypoint")
 echo "rpc : $rpc_url"
@@ -23,7 +51,7 @@ echo "Moving 1000 SOLs to new solana identity"
 
 solana transfer --keypair faucet.json \
   --url $rpc_url \
-  --allow-unfunded-recipient validator/identity.json 1000
+  --allow-unfunded-recipient $VALIDATOR_DIR/identity.json 1000
 
 echo "Moving 100000 SOLs to a random user"
 
@@ -35,24 +63,27 @@ solana transfer --keypair faucet.json \
 echo "Creating vote account for the validator"
 
 solana create-vote-account \
-    --keypair validator/identity.json \
+    --keypair $VALIDATOR_DIR/identity.json \
     --url $rpc_url \
-    validator/vote.json validator/identity.json validator/withdrawer.json
+    $VALIDATOR_DIR/vote.json $VALIDATOR_DIR/identity.json $VALIDATOR_DIR/withdrawer.json
 
 echo "Staring the validator"
+
+rm $VALIDATOR_DIR/log.txt
 
 solana-validator \
     --max-genesis-archive-unpacked-size 1073741824 \
     --no-poh-speed-test \
     --no-os-network-limits-test \
     --entrypoint "$gossip_entrypoint" \
-    --identity validator/identity.json \
-    --vote-account validator/vote.json \
-    --ledger validator/ledger \
-    --log validator/log.txt \
+    --identity $VALIDATOR_DIR/identity.json \
+    --vote-account $VALIDATOR_DIR/vote.json \
+    --ledger $VALIDATOR_DIR/ledger \
+    --log $VALIDATOR_DIR/log.txt \
     --full-rpc-api \
     --no-incremental-snapshots \
     --require-tower \
-    "${@:2}" &
+    --dynamic-port-range $dynamic_port_range \
+    "${@:3}" &
 
 
