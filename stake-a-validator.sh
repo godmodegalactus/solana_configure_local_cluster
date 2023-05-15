e354cce1ba1034836a1625d81bb806149e4e75f0
Godmode Galactus <godmodegalactus@gmail.com>
2023-05-15 11:09:05 +0200

    In stake a validator script, create random user and transfer required funds

--- stake-a-validator.sh
+++ stake-a-validator.sh
@@ -30,6 +30,11 @@ if [[ -z $rpc_url ]]; then
 rpc_url="http://127.0.0.1:8899"
 fi
 
+solana-keygen new --no-passphrase -o random_user.json
+
+USER=$(solana address -k random_user.json)
+solana transfer $USER $STAKE_AMOUNT -k faucet.json -u $rpc_url --allow-unfunded-recipient
+
 solana-keygen new --no-passphrase -o tmp.json
 solana create-stake-account tmp.json $STAKE_AMOUNT -k random_user.json -u $rpc_url
 stake_addr=$(solana address -k tmp.json)
